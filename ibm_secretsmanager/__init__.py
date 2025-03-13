711fc643b8b539bcffda85f15186102bbc99d188
Daniel Chiquito <daniel.chiquito@gmail.com>
2025-03-13 12:22:56 -0400

    First draft

--- ibm_secretsmanager/__init__.py
+++ ibm_secretsmanager/__init__.py
@@ -1,74 +1,56 @@
 import collections
+from urllib.parse import urljoin
 
-CredentialPlugin = collections.namedtuple('CredentialPlugin', ['name', 'inputs', 'backend'])
+import requests
 
-def some_lookup_function(**kwargs):
-    #
-    # IMPORTANT:
-    # replace this section of code with Python code that *actually*
-    # interfaces with some third party credential system
-    # (*this* code is just provided for the sake of example)
-    #
-    url = kwargs.get('url')
-    token = kwargs.get('token')
-    identifier = kwargs.get('identifier')
+CredentialPlugin = collections.namedtuple(
+    "CredentialPlugin", ["name", "inputs", "backend"]
+)
 
-    if token != 'VALID':
-        raise ValueError('Invalid token!')
 
-    value = {
-        'username': 'mary',
-        'email': 'mary@example.org',
-        'password': 'super-secret'
-    }
+def lookup_secret(**kwargs):
+    base_url = kwargs.get("url")
+    iam_token = kwargs.get("iam_token")
+    id = kwargs.get("id")
 
-    if identifier in value:
-        return value[identifier]
+    print(base_url, iam_token, id)
 
-    raise ValueError(f'Could not find a value for {identifier}.')
+    session = requests.Session()
+    session.headers["Authorization"] = f"Bearer {iam_token}"
+    session.headers["Accept"] = "application/json"
+    url = urljoin(base_url, f"/api/v2/secrets/${id}")
+    response = session.get(url)
+    # raise_for_status(response)
+    json = response.json()
+    print(json)
+    return json["payload"]
+
 
 ibm_secretsmanager_plugin = CredentialPlugin(
-    'IBM Secrets Manager AWX Credential Plugin',
-    # see: https://docs.ansible.com/ansible-tower/latest/html/userguide/credential_types.html
-    # inputs will be used to create a new CredentialType() instance
-    #
-    # inputs.fields represents fields the user will specify *when they create*
-    # a credential of this type; they generally represent fields
-    # used for authentication (URL to the credential management system, any
-    # fields necessary for authentication, such as an OAuth2.0 token, or
-    # a username and password). They're the types of values you set up _once_
-    # in AWX
-    #
-    # inputs.metadata represents values the user will specify *every time
-    # they link two credentials together*
-    # this is generally _pathing_ information about _where_ in the external
-    # management system you can find the value you care about i.e.,
-    #
-    # "I would like Machine Credential A to retrieve its username using
-    # Credential-O-Matic B at identifier=some_key"
+    "IBM Secrets Manager",
     inputs={
-        'fields': [{
-            'id': 'url',
-            'label': 'Server URL',
-            'type': 'string',
-        }, {
-            'id': 'token',
-            'label': 'Authentication Token',
-            'type': 'string',
-            'secret': True,
-        }],
-        'metadata': [{
-            'id': 'identifier',
-            'label': 'Identifier',
-            'type': 'string',
-            'help_text': 'The name of the key in My Credential System to fetch.'
-        }],
-        'required': ['url', 'token', 'secret_key'],
+        "fields": [
+            {
+                "id": "url",
+                "label": "Server URL",
+                "type": "string",
+            },
+            {
+                "id": "iam_token",
+                "label": "IAM Token",
+                "type": "string",
+                "secret": True,
+            },
+        ],
+        "metadata": [
+            {
+                "id": "identifier",
+                "label": "Identifier",
+                "type": "string",
+                "help_text": "The name of the key to fetch from IBM Secrets Manager.",
+            }
+        ],
+        "required": ["url", "iam_token", "id"],
     },
-    # backend is a callable function which will be passed all of the values
-    # defined in `inputs`; this function is responsible for taking the arguments,
-    # interacting with the third party credential management system in question
-    # using Python code, and returning the value from the third party
-    # credential management system
-    backend = some_lookup_function
+    backend=lookup_secret,
 )
